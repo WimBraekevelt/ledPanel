3510259f1063278fb4304699e5a7444221c7da32
WimBraekevelt <braekevelt.wim@telenet.be>
2015-05-01 19:31:30 +0200

    start

---
+++ Ledmatrix/LedPanelConstructorForm.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Ledmatrix {
+    partial class LedPanelConstructorForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.groupBoxPanelConstructor = new System.Windows.Forms.GroupBox();
+            this.labelRowsNumber = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBoxPanelConstructor.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxPanelConstructor
+            // 
+            this.groupBoxPanelConstructor.Controls.Add(this.numericUpDown2);
+            this.groupBoxPanelConstructor.Controls.Add(this.numericUpDown1);
+            this.groupBoxPanelConstructor.Controls.Add(this.label2);
+            this.groupBoxPanelConstructor.Controls.Add(this.labelRowsNumber);
+            this.groupBoxPanelConstructor.Location = new System.Drawing.Point(12, 12);
+            this.groupBoxPanelConstructor.Name = "groupBoxPanelConstructor";
+            this.groupBoxPanelConstructor.Size = new System.Drawing.Size(288, 107);
+            this.groupBoxPanelConstructor.TabIndex = 0;
+            this.groupBoxPanelConstructor.TabStop = false;
+            this.groupBoxPanelConstructor.Text = "Create a led panel";
+            this.groupBoxPanelConstructor.Enter += new System.EventHandler(this.groupBoxPanelConstructor_Enter);
+            // 
+            // labelRowsNumber
+            // 
+            this.labelRowsNumber.AutoSize = true;
+            this.labelRowsNumber.Location = new System.Drawing.Point(6, 21);
+            this.labelRowsNumber.Name = "labelRowsNumber";
+            this.labelRowsNumber.Size = new System.Drawing.Size(84, 13);
+            this.labelRowsNumber.TabIndex = 0;
+            this.labelRowsNumber.Text = "Number of rows:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of colums:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(107, 19);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(107, 47);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(63, 20);
+            this.numericUpDown2.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 206);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 206);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // LedPanelConstructorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(668, 332);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBoxPanelConstructor);
+            this.Name = "LedPanelConstructorForm";
+            this.Text = "LedPanelConstructorForm";
+            this.groupBoxPanelConstructor.ResumeLayout(false);
+            this.groupBoxPanelConstructor.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxPanelConstructor;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelRowsNumber;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ Ledmatrix/MainForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ledmatrix{
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+    }
+
+    #region classes
+    public class LedMatrix {
+        private byte identifier;
+        private byte[,] data = new byte[8, 8];
+
+        // Constructor
+        public LedMatrix(byte identifier) :
+            this(identifier, null) {
+        }
+        public LedMatrix(byte identifier, byte[,] data) {
+            this.identifier = identifier;
+            if(this.data != null){
+                this.data = data;
+            }else{
+                this.data = new byte[8, 8] {{0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0},
+                                            {0,0,0,0,0,0,0,0}};
+            }
+        }
+    }
+
+
+    public class LedPanel {
+        private byte identifier;
+        private List<LedMatrix> matrixList = new List<LedMatrix>();
+
+        // Constructor
+        public LedPanel(byte identifier) :
+            this(identifier, new List<LedMatrix>()) {
+        }
+        public LedPanel(byte identifier, List<LedMatrix> matrixList) {
+            this.identifier = identifier;
+            this.matrixList = matrixList;
+        }
+        //
+
+    }
+    #endregion
+}
